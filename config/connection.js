a9ab2d03b92e492a04aa3eb6d78e013c3085ad90
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-11-06 17:07:27 -0500

    seting up with heroku

--- config/connection.js
+++ config/connection.js
@@ -12,14 +12,19 @@ var connection = mysql.createConnection({
 
 var connection = mysql.createConnection(source.localhost);
 
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database:'todoagain_db'
+  });
+};
+
 // Connect to the database
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection
 module.exports = connection;
