77ae69f80c313edad6a7f1ae664cca5859b8e0a8
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-11-07 18:19:20 -0500

    change

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
 USE sofs3ws8waea34a1;
 
 CREATE TABLE burgers (
-id INT AUTO_INCREMENT NOT NULL,
-burger_name VARCHAR (255),
-devoured BOOLEAN NOT NUll,
+id INT AUTO_INCREMENT,
+burger_name VARCHAR(25),
+devoured BOOLEAN,
 PRIMARY KEY(id)
 );
 
