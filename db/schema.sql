5d55db562231dfbfc5c048cc24b559d03d3ad973
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-11-07 17:57:17 -0500

    change

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,9 @@
--- DROP DATABASE IF EXISTS burgers_db;
--- CREATE DATABASE burgers_db;
-
 USE sofs3ws8waea34a1;
 
-DROP TABLE IF EXISTS burgers;
-CREATE TABLE burgers
-(
-id INT AUTO_INCREMENT,
-burger_name VARCHAR (25),
-devoured BOOLEAN,
+CREATE TABLE burgers (
+id INT AUTO_INCREMENT NOT NULL,
+burger_name VARCHAR (255),
+devoured BOOLEAN NOT NUll,
 PRIMARY KEY(id)
 );
 
